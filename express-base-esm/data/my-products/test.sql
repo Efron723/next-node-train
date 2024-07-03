f7b3e2687e5881beddee6b4d8a2089edcc73e563
Efron <a161026521@gmail.com>
2024-07-03 16:00:37 +0800

    sql search formula

--- express-base-esm/data/my-products/test.sql
+++ express-base-esm/data/my-products/test.sql
@@ -13,7 +13,7 @@ WHERE brand = 'Apple';
 SELECT * FROM `my_product`
 WHERE brand IN ('Apple', 'Google');
 
--- 用OR
+-- 複選: 用OR
 SELECT * FROM `my_product`
 WHERE brand ='Apple' OR brand = 'Google';
 
@@ -25,7 +25,30 @@ WHERE name LIKE '%so%';
 SELECT * FROM `my_product`
 WHERE price BETWEEN 5000 AND 15000;
 
--- 用大於小於組合
+-- 價格: 用大於+小於組合
 SELECT * FROM `my_product`
 WHERE price >= 5000 AND price <= 15000;
 
+-- WHERE從句整合測試，每個條件間都是用AND相連
+SELECT * FROM `my_product`
+WHERE brand IN ('Apple', 'Google')
+AND name LIKE '%so%'
+AND price >= 5000 AND price <= 15000;
+
+-- 排序: 價格由低到高排序(順向(1234) asc, 逆向(4321) desc) (查詢字串qs: sort=price&order=asc)
+SELECT * FROM `my_product`
+WHERE brand IN ('Apple', 'Google')
+ORDER BY price ASC;
+
+-- 分頁: 目前第page頁，每頁perpage個(查詢字串qs: page=2&perpage=5)
+-- 公式: limit = perpage
+-- offset 範例: page=1 offset=0, page=2 offset=perpage*1
+-- 公式: offset = (page-1)*perpage
+SELECT * FROM `my_product`
+WHERE brand IN ('Apple', 'Google')
+LIMIT 5 OFFSET 5;
+
+-- 額外需要: 計算在此條件(WHERE從句條件)下，總共有多少筆資料結果(回傳到前端，要呈現有多少頁)
+SELECT COUNT(*) AS COUNT
+FROM `my_product`
+WHERE brand IN ('Apple', 'Google');
